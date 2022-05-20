d562f3bae5746451921e05bbac5ed0ee3bd1bf8e
Siyovush <paraparadox@protonmail.com>
2022-05-21 00:15:36 +0500

    Remove unnecessary files. Set up the styling.

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+
+export default Header
