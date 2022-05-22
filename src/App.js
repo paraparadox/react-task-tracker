d560c946b0c2618cfe246db1cf6fc6da27ee1f23
Siyovush <paraparadox@protonmail.com>
2022-05-22 22:35:17 +0500

    Conditional styling. Toggle reminder for task

--- src/App.js
+++ src/App.js
@@ -24,14 +24,29 @@ function App() {
     }
   ])
 
+  // Delete a Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === id 
+        ? {...task, reminder: !task.reminder} 
+        : task
+      )
+    )
   }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={ tasks } onDelete={ deleteTask }/>
+      { tasks.length > 0 
+        ? (<Tasks tasks={ tasks } onDelete={ deleteTask } onToggle={ toggleReminder }/>)
+        : ('No Tasks to Show')
+      }
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 import Task from './Task'
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={ task.id } task={ task } onDelete={ onDelete }/>
+        <Task key={ task.id } task={ task } onDelete={ onDelete } onToggle={ onToggle }/>
       ))}
     </>
   )
