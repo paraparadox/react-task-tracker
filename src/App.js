f96b2c6d809a2b2598b355dfe02a2ac3ccf3fd4a
Siyovush <paraparadox@protonmail.com>
2022-05-22 19:51:58 +0500

    Delete task and prop drilling

--- src/App.js
+++ src/App.js
@@ -24,10 +24,14 @@ function App() {
     }
   ])
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={ tasks }/>
+      <Tasks tasks={ tasks } onDelete={ deleteTask }/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 import Task from './Task'
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task}/>
+        <Task key={ task.id } task={ task } onDelete={ onDelete }/>
       ))}
     </>
   )
