b175e1606569125138ff010408e632db442ce5b9
Siyovush <paraparadox@protonmail.com>
2022-05-23 17:52:09 +0500

    Toggle adding task form visibility

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -50,12 +52,15 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={ addTask } />
+      <Header onAdd={ () => setShowAddTask(!showAddTask) } showAdd={ showAddTask }/>
+  
+      { showAddTask && (<AddTask onAdd={ addTask } />) }
+  
       { tasks.length > 0 
         ? (<Tasks tasks={ tasks } onDelete={ deleteTask } onToggle={ toggleReminder }/>)
         : ('No Tasks to Show')
       }
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,11 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 
-const Header = ({ title }) => {
-  const onClick = () => {
-      console.log('custom click')
-  }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+        <Button color={ showAdd ? 'red' : 'green' } text={ showAdd ? 'Close' : 'Add'} onClick={ onAdd }/>
     </header>
   )
 }
