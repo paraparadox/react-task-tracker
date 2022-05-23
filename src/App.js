f4c363769af24902dc8ef5c36fc84a4a4c355e8d
Siyovush <paraparadox@protonmail.com>
2022-05-23 18:10:08 +0500

    Install and set up json server

--- src/App.js
+++ src/App.js
@@ -6,26 +6,7 @@ import AddTask from './components/AddTask';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Weekly meeting',
-        day: 'May 21st at 6:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Talk to akai Alisher',
-        day: 'May 21st at 5:00pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Sunday cycling',
-        day: 'May 22nd at 6:00am',
-        reminder: false,
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   // Add a Task
   const addTask = (task) => {
