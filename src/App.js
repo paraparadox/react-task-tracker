339001ed34b564af2e097400bf3f590bebea24e9
Siyovush <paraparadox@protonmail.com>
2022-05-23 18:31:33 +0500

    Deleting task from the backend

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ function App() {
   }
 
   // Delete a Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
