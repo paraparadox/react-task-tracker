afbc455a559e9c3fcac3d0e965517d1cee3be07d
Siyovush <paraparadox@protonmail.com>
2022-05-24 01:42:11 +0500

    Hide Add button on non-root pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color={ showAdd ? 'red' : 'green' } text={ showAdd ? 'Close' : 'Add'} onClick={ onAdd }/>
+        { location.pathname === '/' && <Button color={ showAdd ? 'red' : 'green' } text={ showAdd ? 'Close' : 'Add'} onClick={ onAdd }/>}
     </header>
   )
 }
