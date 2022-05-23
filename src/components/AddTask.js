e8a6bd0f42a3bc0a3ff942890c5f15b2f841477e
Siyovush <paraparadox@protonmail.com>
2022-05-23 17:03:43 +0500

    Form input states (controlled components). Component-level states

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,21 +1,26 @@
+import { useState } from 'react'
 
 const AddTask = () => {
+  const [text, setText] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState(false)
+
   return (
     <form className="add-form">
       
       <div className="form-control">
           <label>Task</label>
-          <input type="text" placeholder="Add Task"/>
+          <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)}/>
       </div>
 
       <div className="form-control">
           <label>Day & Time</label>
-          <input type="text" placeholder="Day & Time"/>
+          <input type="text" placeholder="Day & Time" value={day} onChange={(e) => setDay(e.target.value)}/>
       </div>
 
       <div className="form-control form-control-check">
           <label>Set Reminder</label>
-          <input type="checkbox"/>
+          <input type="checkbox"  value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
       </div>
 
       <input type="submit" value="Save Task" className="btn btn-block" />
