9a8ebc8e4117287ae211b17b07bfd0bedf81b2cc
Siyovush <paraparadox@protonmail.com>
2022-05-24 00:24:55 +0500

    Post request to the backend while creating a task

--- src/App.js
+++ src/App.js
@@ -26,10 +26,17 @@ function App() {
   }
 
   // Add a Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   // Delete a Task
